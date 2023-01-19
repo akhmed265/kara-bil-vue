4cb4a95d55e9428292a2f3aec71552bab0cc7ad0
Akhmed <ahmedmalaciev4@gmail.com>
2023-01-19 20:56:04 +0300

    Footer

--- kara bil/src/App.vue
+++ kara bil/src/App.vue
@@ -5,6 +5,8 @@
     <div class="page-content">
 
     </div>
+
+    <the-footer />
   </div>
 </template>
 
@@ -12,11 +14,14 @@
 import { defineComponent } from 'vue';
 
 import TheHeader from "./components/TheHeader/index.vue";
+import TheFooter from "./components/TheFooter/index.vue";
+
 export default defineComponent({
   name: 'App',
 
     components: {
-      TheHeader
+      TheHeader,
+      TheFooter
     }
 })
 </script>
---
+++ kara bil/src/components/TheFooter/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="the-footer">
+    <div class="container">
+      <div class="the-footer__inner">
+        <img src="../../assets/img/cooperation.svg" alt="" class="the-footer__cooperating">
+        <img src="../../assets/img/telegram.svg" alt="" class="the-footer__telegram">
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'TheFooter'
+})
+</script>
+
+<style lang="scss" scoped>
+.the-footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: var(--color-dark);
+  min-height: 67px;
+
+  &__inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 0;
+    height: 100%;
+  }
+
+
+}
+</style>
